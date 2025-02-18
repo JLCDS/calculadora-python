ba2d772b0c053a2c9f139d63b6f24c5502110020
Juan Lopez <juanlopezcastrods@gmail.com>
2025-02-17 21:46:14 -0500

    fix funtion division

--- calculadora.py
+++ calculadora.py
@@ -9,16 +9,31 @@ Menu Principal  -> elija una opcion
 \n 5. Salir
 """)
 while not fin:
-    option = int (input("opcion: "))
-    if (option == 1):
-        suma1 = int (input ("ingrese el primer numero: "))
-        suma2 = int (input ("ingrese el segundo numero: "))
-        print ("la suma es: ", suma1 + suma2)
-    elif (option == 2):
-        resta1 = int (input ("ingrese el primer numero: "))
-        resta2 = int (input ("ingrese el segundo numero: "))
-        print ("la resta es: ", resta1 - resta2)
-    elif (option == 3):
-        multi1 = int (input ("ingrese el primer numero: "))
-        multi2 = int (input ("ingrese el segundo numero: "))
-        print ("la multiplicacion es: ", multi1 * multi2) 
+    try:
+        option = int (input("opcion: "))
+        if (option == 1):
+            suma1 = int (input ("ingrese el primer numero: "))
+            suma2 = int (input ("ingrese el segundo numero: "))
+            print ("la suma es: ", suma1 + suma2)
+        elif (option == 2):
+            resta1 = int (input ("ingrese el primer numero: "))
+            resta2 = int (input ("ingrese el segundo numero: "))
+            print ("la resta es: ", resta1 - resta2)
+        elif (option == 3):
+            multi1 = int (input ("ingrese el primer numero: "))
+            multi2 = int (input ("ingrese el segundo numero: "))
+            print ("la multiplicacion es: ", multi1 * multi2) 
+        elif (option == 4):
+            div1 = int (input ("ingrese el primer numero: "))
+            div2 = int (input ("ingrese el segundo numero: "))
+            try:
+                print ("la division es: ", div1 / div2)
+            except ZeroDivisionError:
+                print ("no se puede dividir por cero")
+
+        elif (option == 5):
+            fin = True
+        else:
+            print ("opcion invalida")
+    except ValueError:
+        print ("Error debe ingresar un numero")
